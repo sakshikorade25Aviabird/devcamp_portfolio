a00d5f80cf7711eec478e87145fcb457ab92829f
Sakshi Korade <sakshi@aviabird.com.com>
2020-07-20 10:47:25 +0530

    Created seeds file for sample data

--- db/migrate/20200717082241_create_blogs.rb
+++ db/migrate/20200717082241_create_blogs.rb
@@ -8,3 +8,5 @@ class CreateBlogs < ActiveRecord::Migration[5.1]
     end
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_060236) do
+ActiveRecord::Schema.define(version: 2020_07_19_123530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_07_19_060236) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "portfolios", force: :cascade do |t|
