2b03b6a6f16acc1bc1d6df6979b9b4b62a858d9d
Sakshi Korade <sakshi@aviabird.com.com>
2020-07-20 13:48:35 +0530

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,5 +4,40 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all 
   end
 
+  def new 
+   @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio items was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end	
+  
+  def update
+    respond_to do |format|
+      @portfolio_item = Portfolio.find(params[:id])
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolios_pathoS.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
  
